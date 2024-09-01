771b67d6c735bdba1685154b8afec793eb2263d2
Ray, RueiYu Huang <b98170022@gmail.com>
2024-09-01 17:13:21 +0800

    add commonSearch

---
+++ src/components/common/Search.vue
@@ -0,0 +1,43 @@
+<script setup>
+import iconMagnifier from '@/assets/icon/magnifier.svg'
+import iconXmark from '@/assets/icon/xmark.svg'
+const props = defineProps({
+  modelValue: { type: String, default: '', required: true },
+  placeholder: { type: String, default: '', required: false },
+})
+const emit = defineEmits(['update:modelValue', 'submit'])
+
+const handleSubmit = () => {
+  emit('submit', props.modelValue)
+}
+
+const clearInput = () => {
+  emit('update:modelValue', '')
+}
+</script>
+<template>
+  <div class="flex h-12 items-center overflow-hidden rounded-[10px] bg-grey">
+    <div class="relative h-full flex-auto">
+      <input
+        type="text"
+        class="h-full w-full bg-grey pl-2 pr-8 caret-primary-500 placeholder:text-grey-400 focus:outline-none"
+        :value="modelValue"
+        :placeholder="placeholder"
+        @input="$emit('update:modelValue', $event.target.value)"
+      />
+      <img
+        v-if="modelValue.length"
+        alt="magnifier"
+        class="absolute right-2 top-1/2 h-5 -translate-y-1/2 cursor-pointer"
+        :src="iconXmark"
+        @click="clearInput"
+      />
+    </div>
+    <div
+      class="flex aspect-square h-full cursor-pointer items-center justify-center rounded-[10px] bg-primary-500"
+      @click="handleSubmit"
+    >
+      <img alt="magnifier" class="h-7" :src="iconMagnifier" />
+    </div>
+  </div>
+</template>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,28 +1,31 @@
 <script setup>
-import axios from "../utils/axios";
-import { storeToRefs } from "pinia";
-import { useCounterStore } from "../stores/useCountStore.js";
-import { ref, onMounted } from "vue";
-const counterSrore = useCounterStore();
-const { increment } = counterSrore;
-const { count } = storeToRefs(counterSrore);
+import axios from '../utils/axios'
+import { storeToRefs } from 'pinia'
+import { useCounterStore } from '../stores/useCountStore.js'
+import { ref, onMounted } from 'vue'
+import CommonSearch from '@/components/common/Search.vue'
+const counterSrore = useCounterStore()
+const { increment } = counterSrore
+const { count } = storeToRefs(counterSrore)
 
-const data = ref([]);
+const data = ref([])
 
 onMounted(async () => {
   const res = await axios.get(
-    "https://mocki.io/v1/ceb6202e-68d1-46b9-9b8e-737322014290",
-  );
-  data.value = res.data;
-});
+    'https://mocki.io/v1/ceb6202e-68d1-46b9-9b8e-737322014290',
+  )
+  data.value = res.data
+})
+const inputData = ref('')
 </script>
 <template>
   <div>
     <h1>Home</h1>
     <p>Count is: {{ count }}</p>
-    <button class="rounded border border-black p-1" @click="increment">
+    <button class="border-black rounded border p-1" @click="increment">
       Increment
     </button>
     <div>{{ data }}</div>
+    <CommonSearch v-model="inputData" placeholder="輸入搜尋" />
   </div>
 </template>
