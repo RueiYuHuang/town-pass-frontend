fc3cf21490d3486d3d8bca0425bd9a34bafc4dc2
bill <zeng.cae37@gmail.com>
2024-09-07 19:32:04 +0800

    chore: Update Vite configuration to use dynamic port from environment variable

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,10 @@ import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import { createHtmlPlugin } from 'vite-plugin-html';
 import path from 'path'
+import dotenv from 'dotenv';
+
+// Load environment variables from .env file
+dotenv.config();
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -10,7 +14,7 @@ export default defineConfig({
     createHtmlPlugin({
       inject: {
         data: {
-          googleMapsApiKey: process.env.VITE_GOOGLE_MAPS_API_KEY,
+          googleMapsApiKey: process.env.VITE_GOOGLE_MAPS_API_KEY
         },
       },
     }),
@@ -21,7 +25,7 @@ export default defineConfig({
     },
   },
   server: {
-    host: process.env.VITE_HOST_URL,
-    port: process.env.VITE_HOST_PORT,
+    host: process.env.VITE_HOST,
+    port: process.env.VITE_PORT,
   },
 })
