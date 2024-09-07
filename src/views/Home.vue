a1d095ad15f2462af68a964b96c44b96a7ab7e14
bill <zeng.cae37@gmail.com>
2024-09-07 20:17:11 +0800

    chore: Add useConnectionMessage and useHandleConnectionData composables

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -3,6 +3,8 @@ import axios from '../utils/axios'
 import { storeToRefs } from 'pinia'
 import { useCounterStore } from '../stores/useCountStore.js'
 import { ref, onMounted } from 'vue'
+import { useConnectionMessage } from '@/composables/useConnectionMessage';
+import { useHandleConnectionData } from '@/composables/useHandleConnectionData';
 
 const counterSrore = useCounterStore()
 const { increment } = counterSrore
@@ -10,7 +12,17 @@ const { count } = storeToRefs(counterSrore)
 
 const data = ref([])
 
+const handleLocation = (event) => {
+  const result = JSON.parse(event.data);
+  data.value = result
+};
+
+function callFlutter(action, data = null) {
+  useConnectionMessage(action, data)
+};
+
 onMounted(async () => {
+  useHandleConnectionData(handleLocation);
   const res = await axios.get('/api/posts')
   data.value = res.data
 })
@@ -20,6 +32,7 @@ onMounted(async () => {
     <h1>Home</h1>
     <RouterLink to="/style">To Style</RouterLink>
     <p>Count is: {{ count }}</p>
+    <button class="border-black rounded border p-1" @click="callFlutter('location', '111')">locationTrack</button>
     <button class="border-black rounded border p-1" @click="increment">Increment</button>
     <div>{{ data }}</div>
   </div>
